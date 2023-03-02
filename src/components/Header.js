579248163c18a850a132ae23e81de6c213de3081
Ethan Ayaay <ayaayethan@gmail.com>
2023-03-02 14:08:55 -0800

    Added anchor tags to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="flex justify-between bg-orange-200 h-20 opacity-60 shadow-lg p-3">
+    <header className="flex justify-between items-center bg-orange-200 h-20 opacity-60 shadow-lg p-3">
       <h1>Ethan Ayaay</h1>
-      <button className="bg-slate-50 w-52 rounded-full"> Resume </button>
+      <a> About Me </a>
+      <a> Skills </a>
+      <a> Projects </a>
+      <a> Contact </a>
+      <button className="bg-orange-400 w-52 h-[95%] rounded-full hover:bg-orange-600 border-2 border-orange-300 text-white font-bold"> Resume </button>
     </header>
   )
 }
