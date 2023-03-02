5e14812a5d5ef5b66d51168a96a72e58c4f15f9c
Ethan Ayaay <ayaayethan@gmail.com>
2023-03-01 21:11:47 -0800

    Initialized Tailwind

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../app.css';
+import Header from './Header.js';
+
 
 export default function App() {
   return (
     <div>
-      <h1> Ethan Ayaay Portfolio </h1>
+      <Header />
     </div>
   )
 };
