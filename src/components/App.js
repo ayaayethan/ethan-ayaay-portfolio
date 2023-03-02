61cd09ab59c2e1deaad9fdbbdafedefe98225462
Ethan Ayaay <ayaayethan@gmail.com>
2023-03-01 21:22:42 -0800

    Created skeletons

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../app.css';
 import Header from './Header.js';
+import AboutMe from './AboutMe.js';
+import Skills from './Skills.js';
+import Projects from './Projects.js';
+import Contact from './Contact.js';
 
 
 export default function App() {
   return (
     <div>
       <Header />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   )
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header> Ethan Ayaay </header>
+    <header className="flex justify-between bg-orange-200 h-20 opacity-60 shadow-lg p-3">
+      <h1>Ethan Ayaay</h1>
+      <button className="bg-slate-50 w-52 rounded-full"> Resume </button>
+    </header>
   )
 }
