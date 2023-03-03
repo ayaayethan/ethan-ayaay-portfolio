95d2ea759da134f7701983a2cd817af8a6f4ceb4
Ethan Ayaay <ayaayethan@gmail.com>
2023-03-02 18:37:25 -0800

    Hover transition for profile image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,11 +3,14 @@ import profile from '../profile.jpeg';
 
 export default function AboutMe() {
   return (
-    <div className="bg-orange-400 flex py-10 px-5">
-      <img src={profile} className="rounded-full aspect-square h-96 object-cover" ></img>
-      <article>
-        <h1 className="text-5xl font-bold text-slate-50">Hi, I'm Ethan!</h1>
-        <p className="text-lg text-slate-50">A passionate software engineer who loves to create. Growing up, I've always found joy in creation whether it be through drawing, filmmaking, or songwriting.
+    <div className="bg-gradient-to-r from-orange-400 to-orange-600
+    flex py-10 px-5 max-h-[35vh]">
+      <img src={profile} className="rounded-full aspect-square h-96 object-cover
+      transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-[1.03] duration-300
+      shadow-xl" ></img>
+      <article className="max-h-full overflow-auto ml-7 flex flex-col">
+        <h1 className="text-6xl font-bold text-slate-50">Hi, I'm Ethan!</h1>
+        <p className="text-lg text-slate-50 justify-self-center">A passionate software engineer who loves to create. Growing up, I've always found joy in creation whether it be through drawing, filmmaking, or songwriting.
            I took an interest in software engineering solely through the idea that you can create things with code. I attended Hack Reactor, an advanced software engineering
            immersive, where I honed not only my technical skills in JavaScript, Node, React, and Express, but also my non-technical communication skills through white
            boarding and pair-programming. Moving forward, I'd like to keep developing my software engineering skills even further in order to create more impactful and
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BsDownload } from "react-icons/bs";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center bg-orange-200 h-20 w-full opacity-60 shadow-lg p-3 sticky">
+    <header className="flex justify-between items-center bg-orange-700
+    h-20 w-full shadow-lg p-3 sticky text-white">
       <h1>Ethan Ayaay</h1>
       <a> About Me </a>
       <a> Skills </a>
       <a> Projects </a>
       <a> Contact </a>
-      <button className="bg-orange-400 w-52 h-[95%] rounded-full hover:bg-orange-600 border-2 border-orange-300 text-white font-bold"> Resume </button>
+      <button className="bg-orange-500 w-52 h-[95%] rounded-full
+      hover:bg-orange-400 text-white font-bold flex
+      items-center justify-evenly"> Resume <BsDownload size={25} /> </button>
     </header>
   )
 }
