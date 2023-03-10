4566068326aa13b4c24240faafd6dd97650afe78
Ethan Ayaay <ayaayethan@gmail.com>
2023-03-09 21:19:59 -0800

    Linked resume

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,8 +7,8 @@ export default function AboutMe() {
           <h1 className="text-8xl font-bold text-slate-50 mb-5">Who Am I?</h1>
           <p className="text-xl text-slate-50 justify-self-center w-[85%]">I am a passionate software engineer who loves to create. Growing up, I've always found joy in creation whether it be through drawing,
             filmmaking, animation-- you name it. I took an interest in software engineering solely through the idea that you can create things with code. I attended Hack Reactor, an advanced software engineering
-            immersive, where I honed not only my technical skills in JavaScript, Node, React, and Express, but also my non-technical communication skills through white boarding and pair-programming. Moving forward,
-            I'd like to keep developing my software engineering skills even further in order to create more impactful and fulfilling projects.</p>
+            immersive, where I honed not only my technical skills in JavaScript, Node, React, Express, PostgreSQL, and MongoDB but also my non-technical communication skills through white boarding and pair-programming
+            sessions. Moving forward, I'd like to keep developing my software engineering skills even further in order to create more impactful and fulfilling projects.</p>
         </article>
         <article>
           <p className="text-xl text-slate-50">Oh, and I also make music!</p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BsDownload } from "react-icons/bs";
 
 export default function Header() {
-  const onScroll = (section) => {
+  const scrollTo = (section) => {
     let e = document.getElementById(section);
 
     e.scrollIntoView({ behavior: "smooth"})
@@ -11,20 +11,29 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center bg-orange-800
       h-20 w-full shadow-lg text-white fixed">
-      <a  onClick={()=>{onScroll('main')}} className="pl-5 hover:cursor-pointer">Ethan Ayaay</a>
+      <a  onClick={()=>{scrollTo('main')}}
+          className="pl-5 hover:cursor-pointer">Ethan Ayaay</a>
+
       <nav className="flex justify-evenly w-[40%] h-full">
         <a className="hover:cursor-pointer hover:bg-orange-900 hover:underline
-        h-full w-full flex items-center justify-center" onClick={()=>{onScroll('about')}}> About Me </a>
+        h-full w-full flex items-center justify-center"
+           onClick={()=>{scrollTo('about')}}> About Me </a>
         <a className="hover:cursor-pointer hover:bg-orange-900
-        hover:underline h-full w-full flex items-center justify-center" onClick={()=>{onScroll('skills')}}> Skills </a>
+        hover:underline h-full w-full flex items-center justify-center"
+           onClick={()=>{scrollTo('skills')}}> Skills </a>
         <a className="hover:cursor-pointer hover:bg-orange-900
-        hover:underline h-full w-full flex items-center justify-center" onClick={()=>{onScroll('projects')}}> Projects </a>
+        hover:underline h-full w-full flex items-center justify-center"
+           onClick={()=>{scrollTo('projects')}}> Projects </a>
         <a className="hover:cursor-pointer hover:bg-orange-900
-        hover:underline h-full w-full flex items-center justify-center" onClick={()=>{onScroll('contact')}}> Contact </a>
+        hover:underline h-full w-full flex items-center justify-center"
+           onClick={()=>{scrollTo('contact')}}> Contact </a>
       </nav>
-      <button className="bg-orange-500 w-52 h-[75%] rounded-full
+
+      <a className="bg-orange-500 w-52 h-[75%] rounded-full
       hover:bg-orange-400 text-white font-bold flex
-      items-center justify-evenly mr-5"> Resumé <BsDownload size={25} /> </button>
+      items-center justify-evenly mr-5"
+         href="https://docs.google.com/document/d/13uCLBMetSoVP_WD6vK3VISpHiCP2eu7L7asQ2eseeqo/edit?usp=sharing"
+         target="_blank"> Resumé </a>
     </header>
   )
 }
